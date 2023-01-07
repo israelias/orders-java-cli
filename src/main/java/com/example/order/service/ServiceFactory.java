cdfef98976f631823760bb0431b5e0401045c853
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-07 18:45:18 +0800

    Add factory enum with get/update/insert/delete/total for CLI

---
+++ src/main/java/com/example/order/service/ServiceFactory.java
@@ -0,0 +1,40 @@
+package com.example.order.service;
+
+import com.example.order.util.Commands;
+
+/**
+ * Factory class to get a service instance
+ */
+public class ServiceFactory {
+
+    /**
+     * Gets the correct service class according to the give command
+     * @param cmdEnum Command
+     * @return Service class
+     */
+    public static OrderService get(Commands cmdEnum) {
+        OrderService service;
+
+        switch (cmdEnum) {
+            case GET:
+                service = new GetOrderService();
+                break;
+            case UPDATE:
+                service = new UpdateOrderService();
+                break;
+            case INSERT:
+                service = new InsertOrderService();
+                break;
+            case DELETE:
+                service = new DeleteOrderService();
+                break;
+            case TOTAL:
+                service = new TotalOrderService();
+                break;
+            default:
+                throw new RuntimeException("Invalid command received");
+        }
+
+        return service;
+    }
+}
