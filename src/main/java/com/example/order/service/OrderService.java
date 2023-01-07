fd2b0eecd7667d21c36329378c447c1f69e7be1b
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-07 17:20:24 +0800

    Set generic interface with overridable execute method

---
+++ src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.example.order.service;
+
+import com.example.order.dto.ParamsDto;
+
+/**
+ * Interface for service classes
+ */
+public interface OrderService {
+    String execute(ParamsDto paramsDTO);
+}
