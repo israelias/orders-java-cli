dbcce7174fc0a28a78705dbed8fbc4def018bdc9
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-07 17:28:45 +0800

    Add util to stream lists and with status enums

---
+++ src/main/java/com/example/order/util/OrderStatus.java
@@ -0,0 +1,42 @@
+package com.example.order.util;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Enum that represents the status of an order
+ */
+public enum OrderStatus {
+    CREATED("created"),
+    PAID("paid"),
+    CANCELED("canceled")
+    ;
+
+    private final String status;
+
+    /**
+     * Constructor
+     * @param status An order status
+     */
+    OrderStatus(final String status) {
+        this.status = status;
+    }
+
+    /**
+     * Getter for the status
+     * @return Status as the string
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Method to get all the status values of this Enum as a comma-separated string
+     * @return All the status values as a comma-separated string
+     */
+    public static String listOfValues() {
+        return Arrays.stream(OrderStatus.values())
+                .map(Enum::toString)
+                .collect(Collectors.joining(",")).toLowerCase();
+    }
+}
