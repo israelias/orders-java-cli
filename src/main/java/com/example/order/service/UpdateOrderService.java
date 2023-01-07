07b6cda20082304cdf0931038dc1ee6cfbbd6253
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-07 18:34:03 +0800

    Add UpdateOrderService to execute query via ParamsDTO

---
+++ src/main/java/com/example/order/service/UpdateOrderService.java
@@ -0,0 +1,30 @@
+package com.example.order.service;
+
+import com.example.order.dao.UpdateOrderDao;
+import com.example.order.dto.ParamsDto;
+import com.example.order.util.Database;
+
+/**
+ * Service class to update an order
+ */
+public class UpdateOrderService implements OrderService {
+    private UpdateOrderDao updateOrderDao = new UpdateOrderDao(Database.getInstance());
+
+    /**
+     * Method to execute the service operation
+     * @param  paramsDTO Object with the parameters to execute the service
+     */
+    @Override
+    public String execute(ParamsDto paramsDTO) {
+        String result;
+        int rowsAffected = updateOrderDao.updateOrderStatus(paramsDTO);
+
+        if(rowsAffected <= 0) {
+            result = "No rows affected";
+        } else {
+            result = "Rows affected: " + rowsAffected;
+        }
+
+        return result;
+    }
+}
